3f32b21c9174d34ca7eb39146c0aa2cc363a1b63
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-05-27 22:37:25 +0900

    Initial commit

---
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/TopMenuActivity.kt
@@ -0,0 +1,12 @@
+package com.oliva.verde.android.divercitynewsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top)
+    }
+}
