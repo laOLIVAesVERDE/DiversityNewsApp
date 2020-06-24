582991eaf1d49545687ce9df5b0b148b6719894e
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-06-24 23:13:27 +0900

    Update ApiService.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/ApiService.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/ApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 interface ApiService {
     @GET("/v2/top-headlines/")
     fun getNews(@Query("apiKey") apiKey: String,
-                @Query("q") searchWord : String) : Call<ResponseData>
+                @Query("q", encoded = false) searchWord : String) : Call<ResponseData>
                 //@Query("firstSearchWord") firstSearchWord : String,
                 // @Query("secondSearchWord") secondSearchWord : String)
 }
