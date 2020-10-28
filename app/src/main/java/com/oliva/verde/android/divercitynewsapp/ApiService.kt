d8fe605b67de987e1e4c6c9bd8994cb1b1e354de
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-10-28 22:50:48 +0900

    Update ApiService.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/ApiService.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/ApiService.kt
@@ -1,10 +1,7 @@
 package com.oliva.verde.android.divercitynewsapp
 
-import io.reactivex.Observable
-import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
+import io.reactivex.Single
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -12,10 +9,15 @@ interface ApiService {
     // https://newsapi.org/v2/everything/?apiKey=${apiKey}&q=ダイバーシティ というURLオブジェクトにGETリクエスト
     @GET("/v2/everything/") // アノテーション : ソースコード中に登場する要素（クラスやメソッドなど）に対して、コンパイラや実行環境に伝達したい付加的な情報（メタデータ）を注記する仕組み
     fun getNews(@Query("apiKey") apiKey: String,
-                @Query("q") searchWord : String) : Observable<ResponseData> // 返り値をResponseDataを返すObservable型とする
-                                                                            // Observable : 観測可能なオブジェクト
+                @Query("q") searchWord : String) : Single<Response<List<Article>>> // 返り値をSingle型とする(参照：https://qiita.com/takahirom/items/f3e576e91b219c7239e7)
 
-    /** 返り値をCallオブジェクトとする場合
+    /**
+   @GET("/v2/everything/")
+   fun getNews(@Query("apiKey") apiKey: String,
+   @Query("q") searchWord : String) : Observable<ResponseData> // 返り値をResponseDataを返すObservable型とする
+   // Observable : 観測可能なオブジェクト
+
+   返り値をCallオブジェクトとする場合
     fun getNews(@Query("apiKey") apiKey: String,
                 @Query("q") searchWord : String) : Call<ResponseData> // Call : リクエストを送り、レスポンスを返すインターフェース
 
