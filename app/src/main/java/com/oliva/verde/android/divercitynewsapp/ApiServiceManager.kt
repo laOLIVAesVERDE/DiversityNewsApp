0612476a4e2d27128e71983360fded0e7b9f1758
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-10-30 22:39:44 +0900

    Update ApiServiceManager.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/ApiServiceManager.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/ApiServiceManager.kt
@@ -1,5 +1,6 @@
 package com.oliva.verde.android.divercitynewsapp
 
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
