8c0b1c387e182325ce386ed0af5974b216141022
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-11-01 23:25:03 +0900

    Update ArticleAdapter.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/ArticleAdapter.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/ArticleAdapter.kt
@@ -1,49 +1,31 @@
 package com.oliva.verde.android.divercitynewsapp
 
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.squareup.picasso.Picasso
+import com.oliva.verde.android.divercitynewsapp.databinding.NewsRowBinding
 
-class ArticleAdapter(var articleList : MutableList<Article>) : RecyclerView.Adapter<ArticleAdapter.RecycleListViewHolder>() {
-    private lateinit var holder : RecycleListViewHolder // 変数を lateinit 宣言することにより、non-null な初期化済みの変数として参照することができる
+class ArticleAdapter(var articleList : MutableList<Article>) : RecyclerView.Adapter<ArticleAdapter.BindingHolder>() {
 
     // 各アイテムの画面部品が記述されたレイアウトファイルを元にビューホルダオブジェクトを生成する
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecycleListViewHolder {
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingHolder {
+        val layoutInflater = LayoutInflater.from(parent!!.context)
+        val binding = NewsRowBinding.inflate(layoutInflater, parent, false)
         // 生成したビューホルダをリターンする
-        return holder
+        return BindingHolder(binding)
     }
 
     // ビューホルダ内の各画面部品にデータを割り当てる
-    override fun onBindViewHolder(holder: RecycleListViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: BindingHolder, position: Int) {
         val article = articleList[position]
-        val title = article.title
-        val publishedAt = article.publishedAt
-        holder.titleRow.text = title
-        holder.publishDateRow.text = publishedAt.substring(0..9)
-        Picasso.get().load(article.urlToImage).into(holder.imageRow)
-        // fragmentクラスの違いで処理を分岐
-
+        holder.binding.article = article
+        holder.binding.executePendingBindings()
     }
 
     override fun getItemCount(): Int {
         return articleList.size
     }
 
-    inner class RecycleListViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
-        var imageRow : ImageView
-        var titleRow : TextView
-        var publishDateRow : TextView
-
-        init {
-            imageRow = itemView.findViewById(R.id.image_row)
-            titleRow = itemView.findViewById(R.id.title_row)
-            publishDateRow = itemView.findViewById(R.id.publish_date_row)
-        }
-    }
+    inner class BindingHolder(var binding: NewsRowBinding) :
+        RecyclerView.ViewHolder(binding.root)
 }
