1ec444e6c6b77b385ee9281297dff36049eec4ed
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-11-06 23:53:44 +0900

    記事トップ画像処理の追加

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/ImageLoadingUtil.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/ImageLoadingUtil.kt
@@ -1,4 +1,19 @@
 package com.oliva.verde.android.divercitynewsapp
 
-class ImageLoadingUtil {
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.squareup.picasso.Picasso
+import java.lang.StringBuilder
+
+object ImageLoadingUtil {
+    @BindingAdapter("imageUrl")
+    @JvmStatic
+    fun loadImage(
+        imageView: ImageView,
+        urlToImage : String
+    ) {
+        Picasso.get()
+            .load(urlToImage)
+            .into(imageView)
+    }
 }
