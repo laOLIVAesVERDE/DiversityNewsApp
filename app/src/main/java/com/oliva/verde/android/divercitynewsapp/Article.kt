72838775ec1eb50826b9b302ca8b488fda074d9f
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-12-17 23:23:43 +0900

    Update Article.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/Article.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/Article.kt
@@ -6,7 +6,7 @@ import java.util.*
 
 data class Article(
     val url : String = "",
-    val urlToImage : String = "",
+    val urlToImage : String? = "",
     val publishedAt : String = "",
     val title : String = ""
 )
