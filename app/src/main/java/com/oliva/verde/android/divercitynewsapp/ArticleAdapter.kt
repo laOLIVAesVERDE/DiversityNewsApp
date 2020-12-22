7b4a313a4a10ad1a0b4b158892c2f1916ec1f316
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-12-22 22:31:40 +0900

    Update ArticleAdapter.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/ArticleAdapter.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/ArticleAdapter.kt
@@ -4,11 +4,46 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.oliva.verde.android.divercitynewsapp.databinding.NewsRowBinding
 
 class ArticleAdapter() : RecyclerView.Adapter<ArticleAdapter.BindingHolder>() {
     private var articleList : List<Article>? = null
+
+    fun setArticleList(articleList : List<Article>) {
+
+        if (this.articleList == null) {
+            this.articleList = articleList
+            notifyItemRangeInserted(0, articleList.size)
+        } else {
+            val result = DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+                override fun getOldListSize(): Int {
+                    return  requireNotNull(this@ArticleAdapter.articleList).size
+                }
+
+                override fun getNewListSize(): Int {
+                    return articleList.size
+                }
+
+                override fun areContentsTheSame(
+                    oldItemPosition: Int,
+                    newItemPosition: Int
+                ): Boolean {
+                    val oldList = this@ArticleAdapter.articleList
+                    return oldList?.get(oldItemPosition)?.title == articleList[newItemPosition].title
+                }
+
+                override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                    val newArticle = articleList[newItemPosition]
+                    val oldArticle = articleList[oldItemPosition]
+                    return newArticle.title == oldArticle.title && newArticle.url == oldArticle.url
+                }
+            })
+            this.articleList = articleList
+            result.dispatchUpdatesTo(this)
+        }
+    }
 
     // 各アイテムの画面部品が記述されたレイアウトファイルを元にビューホルダオブジェクトを生成する
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingHolder {
