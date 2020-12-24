2d30a91a695abe461453770defba6d2012a99407
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-12-24 17:28:01 +0900

    画像URLのパース処理を改修

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/model/ImageLoadingUtil.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/model/ImageLoadingUtil.kt
@@ -4,17 +4,11 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.squareup.picasso.Picasso
 
-/**
+
 object ImageLoadingUtil {
-    @BindingAdapter("imageUrl")
     @JvmStatic
-    fun loadImage(
-        imageView: ImageView,
-        urlToImage : String
-    ) {
-        Picasso.get()
-            .load(urlToImage)
-            .into(imageView)
+    @BindingAdapter("imageUrl")
+    fun ImageView.loadImage(url : String?) {
+        Picasso.get().load(url).into(this)
     }
 }
- */
