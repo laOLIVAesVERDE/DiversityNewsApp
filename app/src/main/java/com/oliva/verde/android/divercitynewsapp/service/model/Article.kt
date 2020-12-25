addc2e58e02949183c7f1a773136ff46c5b3f4e9
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-12-25 14:26:30 +0900

    DAOの処理を変更

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/service/model/Article.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/service/model/Article.kt
@@ -5,14 +5,14 @@ import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 import java.util.*
 
-data class Article(
+open class Article(
     @PrimaryKey open var id : String = UUID.randomUUID().toString(),
-    open var url : String = "",
-    open var urlToImage : String? = "",
-    open var publishedAt : String = "",
-    open var title : String = "",
+    open var url : String = "url",
+    open var urlToImage : String? = "urlToImage",
+    open var publishedAt : String = "publishedAt",
+    open var title : String = "title",
     open var isReadFlag : Boolean = false
-) : RealmObject()
+) : RealmObject() {}
     /**
     //@PrimaryKey open var id : String = UUID.randomUUID().toString(),
     open var url: String = "url",
--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/viewmodel/HomeFragmentViewModel.kt
@@ -8,16 +8,21 @@ import androidx.lifecycle.*
 import com.oliva.verde.android.divercitynewsapp.service.model.Article
 import com.oliva.verde.android.divercitynewsapp.R
 import com.oliva.verde.android.divercitynewsapp.service.repository.Repository
-import com.oliva.verde.android.divercitynewsapp.view.callback.ArticleClickCallback
+import com.oliva.verde.android.divercitynewsapp.service.repository.database.StockArticleDao
+import io.realm.Realm
+import io.realm.RealmResults
 import kotlinx.coroutines.launch
 
 
 class HomeFragmentViewModel(application: Application) : AndroidViewModel(application) {
     val LOGTAG = "HomeFragmentViewModel"
 
+    private val realm : Realm = Realm.getDefaultInstance()
+    private val stockArticleDao : StockArticleDao = StockArticleDao(realm)
     private val repository = Repository.instance
     var _articleListLiveData : MutableLiveData<List<Article>> = MutableLiveData()
     val articleListLiveData : LiveData<List<Article>> = _articleListLiveData
+    var stockArticleList : RealmResults<Article> = stockArticleDao.select()
 
     init {
         loadArticles()
@@ -31,6 +36,7 @@ class HomeFragmentViewModel(application: Application) : AndroidViewModel(applica
         if (response.isSuccessful) {
             _articleListLiveData.postValue(response.body()?.articles)
         }
-
     }
+
+
 }
