b764ba9061e3282e90189709b95be54204e0425e
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-02-12 21:21:39 +0900

    画面遷移の処理を変更

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/view/ui/activity/MainActivity.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/view/ui/activity/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.oliva.verde.android.divercitynewsapp.R
 import com.oliva.verde.android.divercitynewsapp.view.ui.fragment.HomeFragment
 import com.oliva.verde.android.divercitynewsapp.view.ui.fragment.StockFragment
@@ -17,11 +19,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top)
+
         val homeFragment = HomeFragment()
         val stockFragment = StockFragment()
 
-        setCurrentFragment(homeFragment)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setLogo(R.drawable.ic_toolbar_icon)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
+        // setCurrentFragment(homeFragment)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        bottomNavigationView.setupWithNavController(navController)
+        /*
         bottomNavigationView.setOnNavigationItemSelectedListener {
             when(it.itemId) {
                 R.id.navi_home -> {
@@ -35,6 +46,9 @@ class MainActivity : AppCompatActivity() {
             }
             false
         }
+
+         */
+
         // findNavController : NavHostに表示するコンテンツを、NavigationGraphを元に切り替える処理をするNavControllerを取得し、画面を遷移させる。
         // NavHostはNavigationGraphの宛先を表示するための空のコンテナのこと。
         // val navController = findNavController(R.id.nav_host_fragment)
@@ -42,12 +56,6 @@ class MainActivity : AppCompatActivity() {
         // そのため、BottomNavigationViewの各menuのidと、navigation_graphの各fragmentのidは対応している必要がある
         // setupWithNavController(bottomNavigationView, navController)
 
-
-
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        toolbar.setLogo(R.drawable.ic_toolbar_icon)
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 
     private fun setCurrentFragment(fragment: Fragment)=
