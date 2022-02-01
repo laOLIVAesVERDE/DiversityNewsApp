4ac5c754be398988e401a3ac4bcb89dfac91f5cb
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-02-01 21:46:04 +0900

    Update MyApplication.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/MyApplication.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/MyApplication.kt
@@ -6,22 +6,4 @@ import com.oliva.verde.android.divercitynewsapp.injection.*
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
-class MyApplication : MultiDexApplication() {
-
-    companion object {
-        @JvmStatic
-        lateinit var appComponent: AppComponent
-    }
-    override fun onCreate() {
-        super.onCreate()
-        setupDagger()
-    }
-
-    private fun setupDagger() {
-        appComponent = DaggerAppComponent.builder()
-                .appComponentModule(AppComponentModule(this))
-                .apiModule(ApiModule())
-                .databaseModule(DatabaseModule())
-                .build()
-    }
-}
+class MyApplication : Application()
