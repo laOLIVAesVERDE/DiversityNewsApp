517be900fc0b37317845670861372a38b339366e
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-02-05 16:43:12 +0900

    Update GetArticlesUseCase.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/domain/use_case/get_articles/GetArticlesUseCase.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/domain/use_case/get_articles/GetArticlesUseCase.kt
@@ -1,9 +1,19 @@
 package com.oliva.verde.android.divercitynewsapp.domain.use_case.get_articles
 
+import com.oliva.verde.android.divercitynewsapp.common.Resource
+import com.oliva.verde.android.divercitynewsapp.domain.model.Article
 import com.oliva.verde.android.divercitynewsapp.domain.repository.ArticleRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GetArticlesUseCase @Inject constructor(
     private val articlesRepository: ArticleRepository
 ) {
+    operator fun invoke(searchWord: String): Flow<Resource<List<Article.ResponseArticle>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val articles = articlesRepository.getArticles(searchWord)
+        }
+    }
 }
