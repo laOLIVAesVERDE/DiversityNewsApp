fdb16afe801c55f6e753a66a7a61297193209f3f
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-02-05 16:45:14 +0900

    Update GetArticlesUseCase.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/domain/use_case/get_articles/GetArticlesUseCase.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/domain/use_case/get_articles/GetArticlesUseCase.kt
@@ -1,10 +1,13 @@
 package com.oliva.verde.android.divercitynewsapp.domain.use_case.get_articles
 
 import com.oliva.verde.android.divercitynewsapp.common.Resource
+import com.oliva.verde.android.divercitynewsapp.data.remote.dto.toArticle
 import com.oliva.verde.android.divercitynewsapp.domain.model.Article
 import com.oliva.verde.android.divercitynewsapp.domain.repository.ArticleRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
 import javax.inject.Inject
 
 class GetArticlesUseCase @Inject constructor(
@@ -13,7 +16,12 @@ class GetArticlesUseCase @Inject constructor(
     operator fun invoke(searchWord: String): Flow<Resource<List<Article.ResponseArticle>>> = flow {
         try {
             emit(Resource.Loading())
-            val articles = articlesRepository.getArticles(searchWord)
+            val articles = articlesRepository.getArticles(searchWord).map { it.toArticle() }
+            emit(Resource.Success<List<Article.ResponseArticle>>(articles))
+        } catch (e: HttpException) {
+            emit(Resource.Error<List<Article.ResponseArticle>>(e.localizedMessage ?: "An unexpected error occurred"))
+        } catch (e: IOException) {
+            emit(Resource.Error<List<Article.ResponseArticle>>("Couldn't reach server. Check your internet connection"))
         }
     }
 }
