ae4002e5812289382cae58d5ff663e3a429e9398
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-02-06 22:18:27 +0900

    Update HomeFragmentViewModel.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/viewmodel/HomeFragmentViewModel.kt
@@ -6,15 +6,19 @@ import androidx.lifecycle.*
 import com.oliva.verde.android.divercitynewsapp.domain.model.Article
 import com.oliva.verde.android.divercitynewsapp.R
 import com.oliva.verde.android.divercitynewsapp.domain.repository.ArticleRepository
+import com.oliva.verde.android.divercitynewsapp.domain.use_case.get_articles.GetArticlesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
 
 @HiltViewModel
-class HomeFragmentViewModel(application: Application) : ViewModel() {
+class HomeFragmentViewModel @Inject constructor(
+    private val getArticlesUseCase: GetArticlesUseCase
+) : ViewModel() {
     companion object {
         val LOGTAG = "HomeFragmentViewModel"
     }
 
-    private val repository = ArticleRepository.instance
     private var _responseArticleListLiveData : MutableLiveData<List<Article.ResponseArticle>> = MutableLiveData()
     val responseArticleListLiveData : LiveData<List<Article.ResponseArticle>> = _responseArticleListLiveData
 
@@ -29,18 +33,14 @@ class HomeFragmentViewModel(application: Application) : ViewModel() {
      * 例：レイアウト用のデータを計算している場合、作業を ViewModel にスコープする必要があるため、
      * 　　ViewModel が消去されると、作業はリソースの消費を避けるために自動的にキャンセルされる。
      */
-    private fun loadArticles() = viewModelScope.launch {
-        Log.d(LOGTAG, "loadArticles called")
-        val response = repository.getNewsArticles(getApplication<Application>().getString(
-            R.string.api_key
-        ), getApplication<Application>().getString(R.string.search_word))
-        if (response.isSuccessful) {
-            _responseArticleListLiveData.postValue(response.body()?.articles)
+    private fun loadArticles() {
+        getArticlesUseCase().onEach { result ->
+            
         }
     }
 
     suspend fun insertTargetArticle(stockArticle: Article.StockArticle) {
-        repository.insertArticle(stockArticle)
+//        repository.insertArticle(stockArticle)
     }
 
 
