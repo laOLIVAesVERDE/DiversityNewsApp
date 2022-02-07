fe917e88b744270ed77290d1fed1e45cbdc545ad
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-02-07 21:51:21 +0900

    Update GetStockArticlesUseCase.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/domain/use_case/get_stock_articles/GetStockArticlesUseCase.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/domain/use_case/get_stock_articles/GetStockArticlesUseCase.kt
@@ -5,6 +5,7 @@ import com.oliva.verde.android.divercitynewsapp.data.local.dto.toStockArticle
 import com.oliva.verde.android.divercitynewsapp.data.remote.dto.toArticle
 import com.oliva.verde.android.divercitynewsapp.domain.model.Article
 import com.oliva.verde.android.divercitynewsapp.domain.repository.ArticleRepository
+import com.oliva.verde.android.divercitynewsapp.domain.repository.StockArticleRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
@@ -12,12 +13,12 @@ import java.io.IOException
 import javax.inject.Inject
 
 class GetStockArticlesUseCase @Inject constructor(
-    private val articlesRepository: ArticleRepository
+    private val stockArticleRepository: StockArticleRepository
 ) {
     operator fun invoke(): Flow<Resource<List<Article.StockArticle>>> = flow {
         try {
             emit(Resource.Loading())
-            val articles = articlesRepository.getStockArticles().map { it.toStockArticle() }
+            val articles = stockArticleRepository.getStockArticles().map { it.toStockArticle() }
             emit(Resource.Success<List<Article.StockArticle>>(articles))
         } catch (e: HttpException) {
             emit(Resource.Error<List<Article.StockArticle>>(e.localizedMessage ?: "An unexpected error occurred"))
