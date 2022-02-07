c4b4ee03f50832fe2a8b73bb9adf046da9567dde
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-02-07 21:31:26 +0900

    Update StockArticleListState.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/stock_article/StockArticleListState.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/stock_article/StockArticleListState.kt
@@ -4,6 +4,6 @@ import com.oliva.verde.android.divercitynewsapp.domain.model.Article
 
 data class StockArticleListState(
     val isLoading: Boolean = false,
-    val articles: List<Article.ResponseArticle> = emptyList(),
+    val articles: List<Article.StockArticle> = emptyList(),
     val error: String = ""
 )
