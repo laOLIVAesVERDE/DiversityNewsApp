c41b6a57487adbce7d2052dbfe480d46878b58f6
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-02-07 21:44:49 +0900

    Create StockArticleRepository.kt

---
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/domain/repository/StockArticleRepository.kt
@@ -0,0 +1,7 @@
+package com.oliva.verde.android.divercitynewsapp.domain.repository
+
+import com.oliva.verde.android.divercitynewsapp.data.local.dto.StockArticleDto
+
+interface StockArticleRepository {
+    suspend fun getStockArticles(): List<StockArticleDto>
+}
