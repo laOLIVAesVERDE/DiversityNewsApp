ad9eaeb0eb12f379ae788b9dac73ad21304f615e
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-02-14 00:03:04 +0900

    Create ArticleListScreen.kt

---
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/article_list/components/ArticleListScreen.kt
@@ -0,0 +1,13 @@
+package com.oliva.verde.android.divercitynewsapp.presentation.article_list.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun ArticleListScreen(
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    
+}
