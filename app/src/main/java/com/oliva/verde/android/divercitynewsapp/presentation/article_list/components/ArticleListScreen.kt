7170adc24d87bbcb27b440078a24c6a3d9e09b3b
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-02-14 00:05:58 +0900

    update

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/article_list/components/ArticleListScreen.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/article_list/components/ArticleListScreen.kt
@@ -2,11 +2,13 @@ package com.oliva.verde.android.divercitynewsapp.presentation.article_list.compo
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 
 @Composable
 fun ArticleListScreen(
     navController: NavController,
+    viewModel: HomeFragmentViewModel = hiltViewModel(),
     modifier: Modifier = Modifier
 ) {
     
--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/stock_article/components/StockArticleListScreen.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/stock_article/components/StockArticleListScreen.kt
@@ -2,11 +2,14 @@ package com.oliva.verde.android.divercitynewsapp.presentation.stock_article.comp
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.oliva.verde.android.divercitynewsapp.presentation.article_list.components.HomeFragmentViewModel
 
 @Composable
 fun StockArticleListScreen(
     navController: NavController,
+    viewModel: StockFragmentViewModel = hiltViewModel(),
     modifier: Modifier = Modifier
 ) {
 }
