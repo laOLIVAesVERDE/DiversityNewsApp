70252f8eca9e9c1c80b4feb33f1c2f1cd1b5f2c1
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-02-14 00:04:57 +0900

    Create StockArticleListScreen.kt

---
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/stock_article/components/StockArticleListScreen.kt
@@ -0,0 +1,12 @@
+package com.oliva.verde.android.divercitynewsapp.presentation.stock_article.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun StockArticleListScreen(
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+}
