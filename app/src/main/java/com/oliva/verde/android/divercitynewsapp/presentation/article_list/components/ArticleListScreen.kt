e731852c90562c5024215945f35b64fa9a0ac9de
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-02-17 22:01:08 +0900

    Update ArticleListScreen.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/article_list/components/ArticleListScreen.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/article_list/components/ArticleListScreen.kt
@@ -2,8 +2,15 @@ package com.oliva.verde.android.divercitynewsapp.presentation.article_list.compo
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 
@@ -15,6 +22,16 @@ fun ArticleListScreen(
 ) {
     val state = viewModel.state.value
     Box(modifier = Modifier.fillMaxSize()) {
-        
+        if (state.error.isNotBlank()) {
+            Text(
+                text = state.error,
+                color = MaterialTheme.colors.error,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .align(Alignment.Center)
+            )
+        }
     }
 }
