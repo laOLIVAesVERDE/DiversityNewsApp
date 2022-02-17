eab911cb22c83e6fbf4264959958b74aa560167b
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-02-17 22:00:11 +0900

    Update ArticleListScreen.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/article_list/components/ArticleListScreen.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/article_list/components/ArticleListScreen.kt
@@ -1,5 +1,7 @@
 package com.oliva.verde.android.divercitynewsapp.presentation.article_list.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -11,5 +13,8 @@ fun ArticleListScreen(
     viewModel: ArticleViewModel = hiltViewModel(),
     modifier: Modifier = Modifier
 ) {
-    
+    val state = viewModel.state.value
+    Box(modifier = Modifier.fillMaxSize()) {
+        
+    }
 }
