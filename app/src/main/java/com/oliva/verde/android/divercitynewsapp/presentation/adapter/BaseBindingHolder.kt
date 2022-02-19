2c5cc17e624f770e7c9d0e056d010438c30944f6
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-02-19 23:17:17 +0900

    update

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/adapter/BaseBindingHolder.kt
+++
@@ -1,9 +0,0 @@
-package com.oliva.verde.android.divercitynewsapp.presentation.adapter
-
-import androidx.databinding.ViewDataBinding
-import androidx.recyclerview.widget.RecyclerView
-import com.oliva.verde.android.divercitynewsapp.domain.model.Article
-
-abstract class BaseBindingHolder(binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
-    abstract fun setViewData(article: Article)
-}
