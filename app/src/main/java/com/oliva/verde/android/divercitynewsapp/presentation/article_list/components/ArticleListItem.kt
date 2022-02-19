758ce317e9c988d5c2b0ce25cfc54c942bee4b13
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-02-19 22:32:36 +0900

    Update ArticleListItem.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/article_list/components/ArticleListItem.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/article_list/components/ArticleListItem.kt
@@ -1,10 +1,14 @@
 package com.oliva.verde.android.divercitynewsapp.presentation.article_list.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.oliva.verde.android.divercitynewsapp.domain.model.Article
 
 @Composable
@@ -13,9 +17,18 @@ fun ArticleListItem(
     onItemClick: () -> Unit
 ) {
     Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onItemClick() }
+            .padding(10.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
     ) {
-
+        Image(
+            painter = rememberImagePainter(article.urlToImage),
+            contentDescription = article.title,
+            modifier = Modifier
+                .size(100.dp)
+                .clip(RoundedCornerShape(4.dp))
+        )
     }
 }
