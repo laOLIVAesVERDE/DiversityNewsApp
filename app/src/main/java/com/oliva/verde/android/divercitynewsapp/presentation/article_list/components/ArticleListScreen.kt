bc8326aabb57c62891c411a36e9bae473d42e2b5
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-02-19 22:04:03 +0900

    Update ArticleListScreen.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/article_list/components/ArticleListScreen.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/article_list/components/ArticleListScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -23,6 +25,11 @@ fun ArticleListScreen(
 ) {
     val state = viewModel.state.value
     Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(state.articles) { article ->
+                
+            }
+        }
         if (state.error.isNotBlank()) {
             Text(
                 text = state.error,
