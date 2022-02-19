aaccc780a6866eeedf4dc14fd74258e5d3d962d6
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-02-19 22:12:58 +0900

    Update ArticleListItem.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/article_list/components/ArticleListItem.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/article_list/components/ArticleListItem.kt
@@ -1,10 +1,21 @@
 package com.oliva.verde.android.divercitynewsapp.presentation.article_list.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.oliva.verde.android.divercitynewsapp.domain.model.Article
 
 @Composable
 fun ArticleListItem(
     article: Article.ResponseArticle,
-    onItemClick: () -> Unit) {
+    onItemClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+    }
 }
