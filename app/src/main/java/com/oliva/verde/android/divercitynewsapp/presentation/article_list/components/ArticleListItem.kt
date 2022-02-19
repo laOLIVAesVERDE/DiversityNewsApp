9aa2613481e3b9930d80d333e9c291207aab473a
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-02-19 22:08:33 +0900

    Create ArticleListItem.kt

---
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/article_list/components/ArticleListItem.kt
@@ -0,0 +1,10 @@
+package com.oliva.verde.android.divercitynewsapp.presentation.article_list.components
+
+import androidx.compose.runtime.Composable
+import com.oliva.verde.android.divercitynewsapp.domain.model.Article
+
+@Composable
+fun ArticleListItem(
+    article: Article.ResponseArticle,
+    onItemClick: () -> Unit) {
+}
