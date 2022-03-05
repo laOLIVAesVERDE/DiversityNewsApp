242cfc86203188c81c022870af34d168ff3302dd
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-03-05 10:25:49 +0900

    Create RemoveArticleUseCase.kt

---
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/domain/use_case/RemoveArticleUseCase.kt
@@ -0,0 +1,21 @@
+package com.oliva.verde.android.divercitynewsapp.domain.use_case
+
+import com.oliva.verde.android.divercitynewsapp.common.Resource
+import com.oliva.verde.android.divercitynewsapp.domain.model.Article
+import com.oliva.verde.android.divercitynewsapp.domain.repository.StockArticleRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class RemoveArticleUseCase @Inject constructor(
+    private val stockArticleRepository: StockArticleRepository
+) {
+    operator fun invoke(stockArticle: Article.StockArticle): Flow<Resource<Boolean>> = flow {
+        try {
+            val dto = stockArticle.toStockArticleDto()
+            stockArticleRepository.add(dto)
+        } catch (e: Throwable) {
+            emit(Resource.Error<Boolean>(e.localizedMessage ?: "An unexpected error occurred"))
+        }
+    }
+}
