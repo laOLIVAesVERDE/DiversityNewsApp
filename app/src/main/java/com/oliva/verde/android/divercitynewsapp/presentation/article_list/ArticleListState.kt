fb334298748bf996d47e11e99717c57312aa6ab4
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2022-03-07 23:21:54 +0900

    Update ArticleListState.kt

--- app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/article_list/ArticleListState.kt
+++ app/src/main/java/com/oliva/verde/android/divercitynewsapp/presentation/article_list/ArticleListState.kt
@@ -4,6 +4,7 @@ import com.oliva.verde.android.divercitynewsapp.domain.model.Article
 
 data class ArticleListState(
     val isLoading: Boolean = false,
+    val isAddSuccess: Boolean = false,
     val articles: List<Article.ResponseArticle> = emptyList(),
     val error: String = ""
 )
